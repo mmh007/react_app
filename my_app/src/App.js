4e25dbaf63dcfaf1d705a572a30791a04ca7340d
mark.ma <1245718734@qq.com>
2020-07-22 11:05:30 +0800

    '环境搭建,欢迎回来！'

---
+++ my_app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          让我们一起来学习 React , 欢迎你的到来！
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
